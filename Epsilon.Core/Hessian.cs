0285e458867a3228aaf05ce81f22ff57478db85c
XBambinoX <maxzax5@proton.me>
2026-09-08 15:38:59 +0300

    feat: add complex Hessian and Laplacian methods for symbolic evaluation

--- Epsilon.Core/Hessian.cs
+++ Epsilon.Core/Hessian.cs
@@ -26,6 +26,24 @@ public static class HessianExtensions
     public static Matrix<double> HessianAt(this Expr expr, params (string Name, double Value)[] point) =>
         expr.HessianAt(expr.GetVariables().OrderBy(v => v, StringComparer.Ordinal).ToArray(), point);
 
+    public static Matrix<Complex> HessianAt(this Expr expr, string[] variables, params (string Name, Complex Value)[] point)
+    {
+        int n = variables.Length;
+        var values = new Complex[n, n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Expr firstDerivative = expr.Differentiate(variables[i]);
+            for (int j = 0; j < n; j++)
+                values[i, j] = firstDerivative.Differentiate(variables[j]).EvaluateComplex(point);
+        }
+
+        return new Matrix<Complex>(variables, values);
+    }
+
+    public static Matrix<Complex> HessianAt(this Expr expr, params (string Name, Complex Value)[] point) =>
+        expr.HessianAt(expr.GetVariables().OrderBy(v => v, StringComparer.Ordinal).ToArray(), point);
+
     private static Dictionary<string, double> ToDictionary((string Name, double Value)[] bindings)
     {
         var dict = new Dictionary<string, double>(bindings.Length);
@@ -40,4 +58,16 @@ public static class HessianExtensions
 
     public static Expr Laplacian(this Expr expr) =>
         expr.Laplacian(expr.GetVariables().OrderBy(v => v, StringComparer.Ordinal).ToArray());
+
+    public static double LaplacianAt(this Expr expr, string[] variables, params (string Name, double Value)[] point) =>
+        expr.Laplacian(variables).Evaluate(ToDictionary(point));
+
+    public static double LaplacianAt(this Expr expr, params (string Name, double Value)[] point) =>
+        expr.LaplacianAt(expr.GetVariables().OrderBy(v => v, StringComparer.Ordinal).ToArray(), point);
+
+    public static Complex LaplacianAt(this Expr expr, string[] variables, params (string Name, Complex Value)[] point) =>
+        expr.Laplacian(variables).EvaluateComplex(point);
+
+    public static Complex LaplacianAt(this Expr expr, params (string Name, Complex Value)[] point) =>
+        expr.LaplacianAt(expr.GetVariables().OrderBy(v => v, StringComparer.Ordinal).ToArray(), point);
 }
